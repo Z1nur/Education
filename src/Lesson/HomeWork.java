6bf8c9510b1ecbf4681544fbeede6e722a82fe6b
Z1nur <zinur@yahoo.com>
2022-12-29 19:26:10 +0400

    Правки тестовые

--- src/Lesson/HomeWork.java
+++ src/Lesson/HomeWork.java
@@ -1,7 +1,7 @@
 package Lesson;
-
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Решение первой задачи!");
     }
 }
+
